e601ba8e6f02325fd2b5a92324559abd687cbf1d
Olek Golus <olekgolus02@gmail.com>
2023-03-02 23:17:16 +0100

    Make color dependent on z position

--- app/components/GradientBackground/GradientBackground.tsx
+++ app/components/GradientBackground/GradientBackground.tsx
@@ -11,6 +11,36 @@ const GradientBackground = () => {
   const ANIMATION_SCALE = 0.2;
   const ANIMATION_INTENSITY = 3;
 
+  const fragmentShader = `
+  varying float vZ;
+  
+  void main() {
+    vec3 colorA = vec3(1, 0.7, 0.73);
+    vec3 colorB = vec3(0.7, 1, 1);
+
+    vec3 normalizedPixel = gl_FragCoord.xyz/0.05;
+    vec3 color = mix(colorA, colorB, vZ);
+
+  gl_FragColor = vec4(color,1.0);
+  }`;
+
+  const vertexShader = `
+  varying float vZ;
+
+  void main()
+  {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    
+    vZ = modelPosition.z;
+
+    gl_Position = projectedPosition;
+  }  
+  `;
+
   const planeGeometry = new THREE.PlaneGeometry(10, 10, SEGMENTS, SEGMENTS);
   const updateWave = generatePerlinNoise(
     planeGeometry,
@@ -26,11 +56,14 @@ const GradientBackground = () => {
   return (
     <mesh
       position={[0, 1, -2]}
-      rotation={[-0.5, 0, 0]}
+      rotation={[-0.0, 0, 0]}
       ref={meshRef}
       geometry={planeGeometry}
     >
-      <shaderMaterial vertexShader='' fragmentShader='' />
+      <shaderMaterial
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+      />
     </mesh>
   );
 };
