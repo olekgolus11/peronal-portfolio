1c7d124da58657c794a9687c12b0388c96f9450d
Olek Golus <olekgolus02@gmail.com>
2023-03-10 21:48:06 +0100

    Init fragment shader and vertex shader

--- app/components/GradientBackground/GradientBackground.tsx
+++ app/components/GradientBackground/GradientBackground.tsx
@@ -1,68 +1,28 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { Mesh } from "three";
-import generatePerlinNoise from "./perlinNoise";
 import * as THREE from "three";
+import vertexShader from "./vertexShader";
+import fragmentShader from "./fragmentShader";
 
 const GradientBackground = () => {
   const meshRef = useRef<Mesh>(null!);
   const SEGMENTS = 100;
-  const ANIMATION_SPEED = 2;
-  const ANIMATION_SCALE = 0.2;
-  const ANIMATION_INTENSITY = 3;
-
-  const fragmentShader = `
-  varying float vZ;
-  
-  void main() {
-    vec3 colorA = vec3(1, 0.7, 0.73);
-    vec3 colorB = vec3(0.7, 1, 1);
-
-    vec3 normalizedPixel = gl_FragCoord.xyz/0.05;
-    vec3 color = mix(colorA, colorB, vZ);
-
-  gl_FragColor = vec4(color,1.0);
-  }`;
-
-  const vertexShader = `
-  varying float vZ;
-
-  void main()
-  {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
-    vec4 viewPosition = viewMatrix * modelPosition;
-    vec4 projectedPosition = projectionMatrix * viewPosition;
-
-    
-    vZ = modelPosition.z;
-
-    gl_Position = projectedPosition;
-  }  
-  `;
-
   const planeGeometry = new THREE.PlaneGeometry(10, 10, SEGMENTS, SEGMENTS);
-  const updateWave = generatePerlinNoise(
-    planeGeometry,
-    ANIMATION_SPEED,
-    ANIMATION_SCALE,
-    ANIMATION_INTENSITY
-  );
 
-  useFrame(() => {
-    updateWave();
-  });
+  useFrame(() => {});
 
   return (
     <mesh
-      position={[0, 1, -1]}
+      position={[0, 0, 0]}
       rotation={[-0.0, 0, 0]}
       ref={meshRef}
       geometry={planeGeometry}
     >
       <shaderMaterial
-        vertexShader={vertexShader}
+        wireframe
         fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
       />
     </mesh>
   );
