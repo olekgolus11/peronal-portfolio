14ecdcc5fd8acfdcaa177ed7717e47d2032fa631
Olek Golus <olekgolus02@gmail.com>
2023-03-22 20:30:21 +0100

    Setup background

--- app/components/GradientBackground/fragmentShader.ts
+++ app/components/GradientBackground/fragmentShader.ts
@@ -9,7 +9,7 @@ void main() {
   vec3 colorTop = vec3(0.333, 0.486, 0.576);
 
 
-  vec3 color = mix(colorBottom, colorTop, v_displacement + 0.99);
+  vec3 color = mix(colorBottom, colorTop, v_displacement + 0.8);
 
   gl_FragColor = vec4(color,1.0);
 }
