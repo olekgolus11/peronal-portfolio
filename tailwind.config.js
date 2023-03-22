8bb1ad22935e09487ba500c55e56bd3de9dd0d1e
Olek Golus <olekgolus02@gmail.com>
2023-03-22 23:25:15 +0100

    Setup app directory, add heading text

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["var(--font-libre-caslon-display)", "ui-serif"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+    },
+    container: {
+      padding: {
+        DEFAULT: "3rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {},
   },
   plugins: [],
