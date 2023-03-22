12fa3d7050b98a16cb8939ae8c14d3da4418a461
Olek Golus <olekgolus02@gmail.com>
2023-03-22 23:42:14 +0100

    Change background color

--- app/components/GradientBackground/fragmentShader.ts
+++ app/components/GradientBackground/fragmentShader.ts
@@ -5,8 +5,8 @@ varying float v_displacement;
 
 void main() {
 
-  vec3 colorBottom = vec3(0.031, 0.125, 0.243);
-  vec3 colorTop = vec3(0.333, 0.486, 0.576);
+  vec3 colorBottom = vec3(0.131, 0.125, 0.243);
+  vec3 colorTop = vec3(0.433, 0.486, 0.576);
 
 
   vec3 color = mix(colorBottom, colorTop, v_displacement + 0.8);
