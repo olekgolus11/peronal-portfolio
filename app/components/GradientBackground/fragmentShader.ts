ae3635ce930cf466269f1e296f7bcf4f2c304369
Olek Golus <olekgolus02@gmail.com>
2023-03-23 23:36:37 +0100

    Work on colors

--- app/components/GradientBackground/fragmentShader.ts
+++ app/components/GradientBackground/fragmentShader.ts
@@ -1,15 +1,17 @@
 const fragmentShader = `
 
 varying float v_displacement;
+uniform vec2 u_resolution;
+uniform float u_time;
 
 
 void main() {
 
-  vec3 colorBottom = vec3(0.131, 0.125, 0.243);
-  vec3 colorTop = vec3(0.433, 0.486, 0.576);
+  vec3 colorBottom = vec3(0.145,0.459,0.988);
+  vec3 colorTop = vec3(0.416,0.067,0.796);
 
 
-  vec3 color = mix(colorBottom, colorTop, v_displacement + 1.8);
+  vec3 color = mix(colorBottom, colorTop, v_displacement * 2.0);
 
   gl_FragColor = vec4(color,1.0);
 }
