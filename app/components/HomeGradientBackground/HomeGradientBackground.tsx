03b533cc4e8b55efcfa5617b89407d19aec59176
Olek Golus <olekgolus02@gmail.com>
2023-03-25 21:53:16 +0100

    Clean up code, create my work section

--- app/components/HomeGradientBackground/HomeGradientBackground.tsx
+++ app/components/HomeGradientBackground/HomeGradientBackground.tsx
@@ -1,48 +1,13 @@
-import { useFrame } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
-import { Mesh } from "three";
-import * as THREE from "three";
-import vertexShader from "./vertexShader";
-import fragmentShader from "./fragmentShader";
+import { Canvas } from "@react-three/fiber";
+import Gradient from "./Gradient";
 
 const HomeGradientBackground = () => {
-  const meshRef = useRef<Mesh>(null!);
-  const SEGMENTS = 1000;
-  const planeGeometry = new THREE.PlaneGeometry(20, 20, SEGMENTS, SEGMENTS);
-  const windowSize = new THREE.Vector2(window.innerWidth, window.innerHeight);
-
-  const uniforms = useMemo(
-    () => ({
-      u_time: {
-        value: 0.0,
-      },
-      u_resolution: {
-        value: windowSize,
-      },
-    }),
-    []
-  );
-
-  useFrame((state) => {
-    const { clock } = state;
-    uniforms.u_time.value = 0.4 * clock.getElapsedTime();
-    uniforms;
-  });
-
   return (
-    <mesh
-      position={[0, 0, 1]}
-      rotation={[0.75, 0, 0]}
-      ref={meshRef}
-      geometry={planeGeometry}
-      scale={4}
-    >
-      <shaderMaterial
-        fragmentShader={fragmentShader}
-        vertexShader={vertexShader}
-        uniforms={uniforms}
-      />
-    </mesh>
+    <div className='fixed w-full h-screen -z-10'>
+      <Canvas camera={{ position: [0, -8, 3] }}>
+        <Gradient />
+      </Canvas>
+    </div>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import "./globals.css";
-import HomeGradientBackground from "./components/HomeGradientBackground/HomeGradientBackground";
-import MyWorkGradientBackground from "./components/MyWorkGradientBackground/MyWorkGradientBackground";
-import { Canvas } from "@react-three/fiber";
 import { Libre_Caslon_Display } from "next/font/google";
+import HomeGradientBackground from "./components/HomeGradientBackground/HomeGradientBackground";
 
-// If loading a variable font, you don't need to specify the font weight
 const libre_caslon_display = Libre_Caslon_Display({
   weight: "400",
   subsets: ["latin"],
@@ -27,17 +24,7 @@ export default function RootLayout({
       */}
       <head />
       <body>
-        <div className='absolute w-full h-screen -z-10'>
-          <Canvas camera={{ position: [0, -8, 3] }}>
-            <HomeGradientBackground />
-          </Canvas>
-          <Canvas
-            camera={{ position: [0, -8, 3] }}
-            style={{ backgroundColor: "#0f0f0f" }}
-          >
-            <MyWorkGradientBackground />
-          </Canvas>
-        </div>
+        <HomeGradientBackground />
         <div>{children}</div>
       </body>
     </html>
