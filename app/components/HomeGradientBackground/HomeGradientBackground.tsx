1e3acb520b9d9c1ccb4f5ee6c0526bdc301c3d62
Olek Golus <olekgolus02@gmail.com>
2023-03-27 19:17:41 +0200

    Change background to blob

--- app/components/HomeGradientBackground/HomeGradientBackground.tsx
+++ app/components/HomeGradientBackground/HomeGradientBackground.tsx
@@ -4,7 +4,10 @@ import Gradient from "./Gradient";
 const HomeGradientBackground = () => {
   return (
     <div className='fixed w-full h-screen -z-10'>
-      <Canvas camera={{ position: [0, -8, 3] }}>
+      <Canvas
+        camera={{ position: [0, 0, 0] }}
+        style={{ backgroundColor: "#0f0f0f" }}
+      >
         <Gradient />
       </Canvas>
     </div>
