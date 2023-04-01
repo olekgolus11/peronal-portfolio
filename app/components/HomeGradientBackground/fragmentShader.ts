d93e44d83e488755987ae77cd2bb820dc7687afc
Olek Golus <olekgolus02@gmail.com>
2023-04-01 11:58:24 +0200

    Edit blob to smoke

--- app/components/HomeGradientBackground/fragmentShader.ts
+++ app/components/HomeGradientBackground/fragmentShader.ts
@@ -7,11 +7,13 @@ uniform float u_time;
 
 void main() {
 
-  vec3 colorBottom = vec3(0.05, 0.05, 0.05);
+  vec3 colorBottom = vec3(0.45, 0.45, 0.45);
+  vec3 colorMid = vec3(0.15, 0.17, 0.19);
   vec3 colorTop = vec3(0.12, 0.14, 0.15);
+  float displacement = v_displacement - 0.5;
 
-
-  vec3 color = mix(colorBottom, colorTop, v_displacement * 1.0);
+  vec3 color = mix(colorBottom, colorMid, displacement * 1.5 );
+ color = mix(color, colorTop, displacement * 2.8 - 0.3);
 
   gl_FragColor = vec4(color,1.0);
 }
