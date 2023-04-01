ad6a1a4fc0de4c56b105b0b8c833c76da886f847
Olek Golus <olekgolus02@gmail.com>
2023-04-01 20:39:12 +0200

    Change LandingSection name

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import LandingPage from "./components/LandingPage/LandingPage";
+import LandingSection from "./components/LandingSection/LandingSection";
 import MyWorkGradientBackground from "./components/MyWorkGradientBackground/MyWorkGradientBackground";
 
 export default function MainPage() {
   return (
     <>
-      <LandingPage />
+      <LandingSection />
       <div className='relative'>
         <p className='text-7xl sm:text-9xl md:text-[10rem] lg:text-[10rem] xl:text-[12rem] font-serif text-slate-50 text-center pt-12'>
           MY WORK
