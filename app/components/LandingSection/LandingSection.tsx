e842a0acc39ef7603a4279c5ec8a8e9fdc0f6bc8
Olek Golus <olekgolus02@gmail.com>
2023-06-17 16:16:44 +0200

    Work on pane

--- app/components/LandingSection/LandingSection.tsx
+++ app/components/LandingSection/LandingSection.tsx
@@ -2,12 +2,8 @@ const LandingSection = () => {
   return (
     <div className='container mx-auto h-screen text-slate-50 text-center'>
       <div className='flex flex-col h-full justify-center'>
-        <p className='text-7xl sm:text-9xl md:text-[10rem] lg:text-[10rem] xl:text-[12rem] font-serif'>
-          Aleksander
-        </p>
-        <p className='text-6xl md:text-8xl lg:text-[9rem] xl:text-[11rem] font-serif'>
-          Golus
-        </p>
+        <p className='text-h1 font-serif'>Aleksander</p>
+        <p className='text-h2 font-serif'>Golus</p>
         <p className='text-4xl font-serif justify-self-end mt-40'>
           Front-end developer
         </p>
--- app/components/ProjectPane/ProjectPane.tsx
+++ app/components/ProjectPane/ProjectPane.tsx
@@ -1,15 +1,38 @@
 const ProjectPane = () => {
+  const projectOverviewText = () => {
+    return (
+      <>
+        <p className='text-slate-50'>
+          Idemian is a web application made for IDEMIA by me and my friends from
+          university.
+        </p>
+        <br />
+        <p className='text-slate-50'>
+          It is an application that is used both for onboarding new employees as
+          well as for entartainment during company events.
+        </p>
+      </>
+    );
+  };
+
   return (
-    <div className='m-14 p-14 h-96 backdrop-blur bg-gray-800 rounded-md bg-clip-padding backdrop-filter bg-opacity-30 border border-gray-100'>
-      <div className='grid grid-cols-12 gap-10 h-full'>
-        <div className='col-span-2 flex flex-col justify-evenly'>
-          <p className='text-slate-50'>Overview</p>
-          <p className='text-slate-50'>Details</p>
-          <p className='text-slate-50'>Screenshots</p>
+    <div className='flex my-14 h-[60vh] page-container pane'>
+      <div className='flex flex-col p-14 gap-14'>
+        <p className='text-white text-h6 font-serif'>IDEMIAN</p>
+        <div className='grid grid-cols-12 gap-10 flex-1'>
+          <div className='col-span-2 flex flex-col justify-evenly'>
+            <p className='text-slate-50'>Overview</p>
+            <p className='text-slate-50'>Details</p>
+            <p className='text-slate-50'>Screenshots</p>
+          </div>
+          <div className='col-span-10'>{projectOverviewText()}</div>
         </div>
-        <div className='col-span-5'></div>
-        <div className='col-span-5'></div>
       </div>
+      <img
+        src='/idemian2.png'
+        alt='idemian app png'
+        className='object-contain border-l rounded-r-md border-gray-100 ml-auto'
+      />
     </div>
   );
 };
