b0904ade9e85ecc6401ce562b80e341a39aecbfa
Olek Golus <olekgolus02@gmail.com>
2023-06-18 16:25:37 +0200

    Update nextjs and react

--- app/components/LandingSection/LandingSection.tsx
+++ app/components/LandingSection/LandingSection.tsx
@@ -1,6 +1,6 @@
 const LandingSection = () => {
   return (
-    <div className='container mx-auto h-screen text-slate-50 text-center'>
+    <div className='container mx-auto h-screen text-center'>
       <div className='flex flex-col h-full justify-center'>
         <p className='text-h1 font-serif'>Aleksander</p>
         <p className='text-h2 font-serif'>Golus</p>
--- app/components/TextSlider/TextSlider.tsx
+++ app/components/TextSlider/TextSlider.tsx
@@ -2,7 +2,7 @@ const TextSlider = ({ text }: { text: string }) => {
   return (
     <div className='overflow-hidden flex whitespace-nowrap'>
       {Array.from({ length: 4 }).map(() => (
-        <p className='text-h1 font-serif text-slate-50 text-center py-12 animate-slide px-2 sm:px-3 md:px-5 lg:px-6'>
+        <p className='text-h1 font-serif text-center py-12 animate-slide px-2 sm:px-3 md:px-5 lg:px-6'>
           {text}
         </p>
       ))}
