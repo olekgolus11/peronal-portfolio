1580aebd5f627323962ab6f4a92162ef88a2088d
Olek Golus <olekgolus02@gmail.com>
2023-06-24 17:23:14 +0200

    Change MY WORK animation to framer motion

--- components/TextSlider/TextSlider.tsx
+++ components/TextSlider/TextSlider.tsx
@@ -1,10 +1,16 @@
+import { motion } from "framer-motion";
+
 const TextSlider = ({ text }: { text: string }) => {
   return (
     <div className='overflow-hidden flex whitespace-nowrap'>
       {Array.from({ length: 4 }).map(() => (
-        <p className='text-h1 font-serif text-center py-12 animate-slide px-2 sm:px-3 md:px-5 lg:px-6'>
+        <motion.p
+          animate={{ x: "-100%" }}
+          transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
+          className='text-h1 font-serif text-center py-12 px-2 sm:px-3 md:px-5 lg:px-6'
+        >
           {text}
-        </p>
+        </motion.p>
       ))}
     </div>
   );
