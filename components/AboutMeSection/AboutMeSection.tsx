11912bce3a51f5afdb841b0fbca777221dfc9044
Olek Golus <olekgolus02@gmail.com>
2023-06-29 12:43:52 +0200

    Seperate paragraphs with unique ids

--- components/AboutMeSection/AboutMeSection.tsx
+++ components/AboutMeSection/AboutMeSection.tsx
@@ -26,23 +26,26 @@ const AboutMeSection = () => {
       );
     };
 
-    animateLine("#about-me-line-1", "6vw", "-9vw");
-    animateLine("#about-me-line-2", "-8vw", "9vw");
-    animateLine("#about-me-line-3", "8vw", "-7vw");
-    animateLine("#about-me-line-4", "-5vw", "8vw");
+    animateLine("#about-me-intro-line-1", "6vw", "-9vw");
+    animateLine("#about-me-intro-line-2", "-8vw", "9vw");
+    animateLine("#about-me-intro-line-3", "8vw", "-7vw");
+    animateLine("#about-me-intro-line-4", "-5vw", "8vw");
   }, []);
 
   const createIntroducingText = () => {
     return (
       <p className='text-h4 font-sans font-light mix-blend-difference'>
-        <div id='about-me-line-1'>
+        <div id='about-me-intro-line-1'>
           Hi! I'm <span className='font-italic'>Aleksander.</span>
         </div>
-        <div id='about-me-line-2'>I'm a 21 years old</div>
-        <div id='about-me-line-3' className='font-italic whitespace-nowrap'>
+        <div id='about-me-intro-line-2'>I'm a 21 years old</div>
+        <div
+          id='about-me-intro-line-3'
+          className='font-italic whitespace-nowrap'
+        >
           front-end developer
         </div>
-        <div id='about-me-line-4'>
+        <div id='about-me-intro-line-4'>
           from <span className='font-italic'>Poland.</span>
         </div>
       </p>
@@ -57,26 +60,26 @@ const AboutMeSection = () => {
       <div className='page-container h-screen flex items-center'>
         {createIntroducingText()}
       </div>
-      <div className='page-container-l flex'>
+      <div className='page-container-l flex gap-16'>
         <div className='text-p-xl font-sans font-light mix-blend-difference'>
-          <div>
+          <div id='about-me-biography-p-1'>
             I've always been a person who wanted to express himself so to match
             my creativity with my passion for technology I started learning 3D
             graphics in blender.
           </div>
           <br />
-          <div>
+          <div id='about-me-biography-p-2'>
             I've had so much fun with it but I wanted to do something more,
             something that I could connect it with my programming skills.
           </div>
           <br />
-          <div>
+          <div id='about-me-biography-p-3'>
             That's why I started learning front-end development and just after
             few months I had an opportunity to work as a front-end developer
             trainee in IDEMIA.
           </div>
           <br />
-          <div>
+          <div id='about-me-biography-p-4'>
             My main goal was to be a fullstack developer and so right after I
             finished my internship I got myself into another one but this time
             as a fullstack developer.
@@ -84,7 +87,7 @@ const AboutMeSection = () => {
         </div>
         <Image
           src={Photo2}
-          className='w-[35vw] max-h-[70vh] ml-auto object-contain aspect-phone'
+          className='w-[30vw] ml-auto object-contain'
           alt='elo'
         />
         {/* <img src='photo2.jpg' className='max-h-[90vh] ml-auto' /> */}
